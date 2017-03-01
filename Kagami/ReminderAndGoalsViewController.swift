d477ece91190d0494dfcb9431a273e80faa8958f
Annie <annie.mmt@gmail.com>
2017-03-01 17:10:30 -0500

    added fonts

--- Kagami/ReminderAndGoalsViewController.swift
+++ Kagami/ReminderAndGoalsViewController.swift
@@ -15,7 +15,7 @@ class ReminderAndGoalsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .blue        
     }
 
     override func didReceiveMemoryWarning() {
