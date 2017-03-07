cfff5051692cc3b6ec22346d8d737db2618b5321
Annie <annie.mmt@gmail.com>
2017-03-07 15:18:13 -0500

    to do textfields

--- Kagami/ToDoView.swift
+++ Kagami/ToDoView.swift
@@ -10,17 +10,18 @@ import Foundation
 import UIKit
 import SnapKit
 
-class ToDoView: UIView, UITableViewDelegate, UITableViewDataSource {
+class ToDoView: UIView, UITextFieldDelegate/*, UITableViewDelegate, UITableViewDataSource*/ {
     
-    var toDoList = ToDo.getMockData()
+//    var toDoList = ToDo.getMockData()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        textFieldOne.delegate = self
         setupView()
-        toDoListTableView.delegate = self
-        toDoListTableView.dataSource = self
-        toDoListTableView.register(ToDoTableViewCell.self, forCellReuseIdentifier: ToDoTableViewCell.identifier)
+//        toDoListTableView.delegate = self
+//        toDoListTableView.dataSource = self
+//        toDoListTableView.register(ToDoTableViewCell.self, forCellReuseIdentifier: ToDoTableViewCell.identifier)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,18 +31,43 @@ class ToDoView: UIView, UITableViewDelegate, UITableViewDataSource {
     // MARK: - Set up Hierarchy & constraints
     
     func setupView() {
-        self.addSubview(toDoListTableView)
+//        self.addSubview(toDoListTableView)
         self.addSubview(button)
+        self.addSubview(backgroundView)
+        self.addSubview(textFieldOne)
+        self.addSubview(textFieldTwo)
+        self.addSubview(textFieldThree)
         
         button.snp.makeConstraints { (view) in
-            view.centerX.bottom.equalToSuperview()
+            view.left.right.bottom.equalToSuperview()
         }
-        toDoListTableView.snp.makeConstraints { (view) in
-            view.top.left.right.equalToSuperview()
-            view.bottom.equalTo(button.snp.top)
+//        toDoListTableView.snp.makeConstraints { (view) in
+//            view.top.left.right.equalToSuperview()
+//            view.bottom.equalTo(button.snp.top)
+//        }
+        backgroundView.snp.makeConstraints { (view) in
+            view.top.bottom.left.right.equalToSuperview()
+        }
+        textFieldOne.snp.makeConstraints { (field) in
+            field.top.equalTo(self.snp.top).inset(100)
+            field.centerX.equalToSuperview()
+            field.height.equalTo(self.snp.height).multipliedBy(0.08)
+            field.width.equalTo(self.snp.width).multipliedBy(0.9)
+        }
+        textFieldTwo.snp.makeConstraints { (field) in
+            field.top.equalTo(textFieldOne.snp.bottom).offset(25)
+            field.centerX.equalToSuperview()
+            field.height.equalTo(self.snp.height).multipliedBy(0.08)
+            field.width.equalTo(self.snp.width).multipliedBy(0.9)
+        }
+        textFieldThree.snp.makeConstraints { (field) in
+            field.top.equalTo(textFieldTwo.snp.bottom).offset(25)
+            field.centerX.equalToSuperview()
+            field.height.equalTo(self.snp.height).multipliedBy(0.08)
+            field.width.equalTo(self.snp.width).multipliedBy(0.9)
         }
     }
-    
+    /*
     // MARK: - TableView
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -65,7 +91,7 @@ class ToDoView: UIView, UITableViewDelegate, UITableViewDataSource {
         }
         
         return cell
-    }
+    }*/
     
     func addItem() {
         print("add to tableview")
@@ -85,5 +111,50 @@ class ToDoView: UIView, UITableViewDelegate, UITableViewDataSource {
         let tableView = UITableView()
         tableView.estimatedRowHeight = 70
         return tableView
+    }()
+    
+    lazy var backgroundView: UIImageView = {
+        let view = UIImageView()
+        let im = UIImage(named: "checklist-bg")
+        view.image = im
+        view.contentMode = .scaleAspectFill
+        view.alpha = 0.6
+        return view
+    }()
+    
+    lazy var textFieldOne: UITextField = {
+        let field = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        field.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        field.placeholder = "TO DO"
+        field.autocorrectionType = .yes
+        field.keyboardType = .default
+        field.returnKeyType = .done
+        field.borderStyle = UITextBorderStyle.none
+        field.contentVerticalAlignment = .center
+        return field
+    }()
+    
+    lazy var textFieldTwo: UITextField = {
+        let field = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        field.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        field.placeholder = "TO DO"
+        field.autocorrectionType = .yes
+        field.keyboardType = .default
+        field.returnKeyType = .done
+        field.borderStyle = UITextBorderStyle.none
+        field.contentVerticalAlignment = .center
+        return field
+    }()
+    
+    lazy var textFieldThree: UITextField = {
+        let field = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        field.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        field.placeholder = "TO DO"
+        field.autocorrectionType = .yes
+        field.keyboardType = .default
+        field.returnKeyType = .done
+        field.borderStyle = UITextBorderStyle.none
+        field.contentVerticalAlignment = .center
+        return field
     }()
 }
